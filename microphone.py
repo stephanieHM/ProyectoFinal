c6c189058d67fdb1e87c06ef38b1233c0125df6c
stephanie <f1n3@hotmail.com>
2018-01-10 13:40:35 -0600

    audio.wav is a file about  microphone.py listens. test.wav is a file about ruido.py generates.

--- microphone.py
+++ microphone.py
@@ -6,12 +6,16 @@ import math
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 from scipy.io import wavfile
 #import urllib  
 #import urllib2 
 import os
+
 class Microphone:
+
     def rms(self,frame):
+
         count = len(frame)/2
         format = "%dh"%(count)
         shorts = struct.unpack( format, frame )
@@ -21,8 +25,12 @@ class Microphone:
             sum_squares += n*n
         rms = math.pow(sum_squares/count,0.5);
         return rms * 1000
-    def passiveListen(self,persona):
-        CHUNK = 1024; RATE = 8000; THRESHOLD = 200; LISTEN_TIME = 5
+
+    def passiveListen(self,persona): 
+	
+	#Espera para el audio, threshold define el umbral
+
+        CHUNK = 1024; RATE = 8000; THRESHOLD = 0; LISTEN_TIME = 12
         didDetect = False
         # prepare recording stream
         p = pyaudio.PyAudio()
@@ -35,8 +43,11 @@ class Microphone:
             input = stream.read(CHUNK)
             rms_value = self.rms(input)
             print rms_value
-            if (rms_value < THRESHOLD):
+            if (rms_value >= THRESHOLD):
                 didDetect = True
+		starting_point = time.time()
+		print ("time is...")		
+		print starting_point
                 print "Listening...\n"
                 break
         if not didDetect:
@@ -45,20 +56,21 @@ class Microphone:
             return False
         # append all the chunks
         all.append(input)
-        for i in range(0, 7):
+        for i in range(0, 100): #El num. 100 ajusta la duracion (aprox. 12 segundos en este caso)
             data = stream.read(CHUNK)
             all.append(data)
         # save the audio data   
         data = ''.join(all)
         stream.stop_stream()
         stream.close()
-        wf = wave.open('audio.wav', 'wb')
+        wf = wave.open('audio.wav', 'wb') #Archivo wav donde se procesa el audio grabado
         wf.setnchannels(1)
         wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
         wf.setframerate(RATE)
         wf.writeframes(data)
         wf.close()
         return True
+
 if __name__ == '__main__':
     mic = Microphone()
     while True:
@@ -68,7 +80,12 @@ if __name__ == '__main__':
             c = np.fft.fft(data) # create a list of complex number
             freq = np.fft.fftfreq(L)
             #freq = np.linspace(0, 1/(2L), L/2)
-            print freq
+            print ("frecuencia 1 ")
+	    print freq
             freq_in_hertz = abs(freq * fs)
+	    print freq_in_hertz
             plt.plot(freq_in_hertz, abs(c))
             plt.show()
+	    final_point = time.time()
+	    print ("time final is...")		
+	    print final_point
---
+++ ruido.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import numpy as np
+ 
+import wave
+ 
+import struct
+ 
+import matplotlib.pyplot as plt
+
+# frequency is the number of times a wave repeats a second
+ 
+frequency = 1000
+ 
+noisy_freq = 50
+ 
+num_samples = 48000
+
+amplitude = 16000
+ 
+file = "test.wav"
+
+ 
+# The sampling rate of the analog to digital convert
+ 
+sampling_rate = 48000.0
+
+#Create the sine wave and noise
+ 
+sine_wave = [np.sin(2 * np.pi * frequency * x1 / sampling_rate) for x1 in range(num_samples)]
+ 
+sine_noise = [np.sin(2 * np.pi * noisy_freq * x1/  sampling_rate) for x1 in range(num_samples)]
+ 
+##Put in a list
+
+nframes=num_samples
+ 
+comptype="NONE"
+ 
+compname="not compressed"
+ 
+nchannels=1
+ 
+sampwidth=2
+
+##Wave file
+
+wav_file=wave.open(file, 'w')
+ 
+wav_file.setparams((nchannels, sampwidth, int(sampling_rate), nframes, comptype, compname))
+
+for s in sine_noise:
+   wav_file.writeframes(struct.pack('h', int(s*amplitude)))
+
+
+#Convert them to numpy arrays
+ 
+sine_wave = np.array(sine_wave)
+ 
+sine_noise = np.array(sine_noise)
+
+# Add them to create a noisy signal
+ 
+combined_signal = sine_wave + sine_noise
+
+
+plt.subplot(3,1,1)
+ 
+plt.title("Original sine wave")
+ 
+# Need to add empty space, else everything looks scrunched up!
+ 
+plt.subplots_adjust(hspace=.5)
+ 
+plt.plot(sine_wave[:500])
+ 
+plt.subplot(3,1,2)
+ 
+plt.title("Noisy wave")
+ 
+plt.plot(sine_noise[:4000])
+ 
+plt.subplot(3,1,3)
+ 
+plt.title("Original + Noise")
+ 
+plt.plot(combined_signal[:3000])
+ 
+plt.show()
+
+
+
+data_fft = np.fft.fft(combined_signal)
+ 
+freq = (np.abs(data_fft[:len(data_fft)]))
+
+
+
+plt.plot(freq)
+ 
+plt.title("Before filtering: Will have main signal (1000Hz) + noise frequency (50Hz)")
+ 
+plt.xlim(0,1200)
